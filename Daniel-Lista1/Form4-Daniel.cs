b56778473ef13af3effddf7bdfa496f0ad3af232
okinawabe <beatriz.felix042007@gmail.com>
2025-03-27 08:17:42 -0300

    v1-2 *3-4

---
+++ Daniel-Lista1/Form4-Daniel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Daniel_Lista1
+{
+    public partial class Form4_Daniel : Form
+    {
+        public Form4_Daniel()
+        {
+            InitializeComponent();
+        }
+    }
+}
