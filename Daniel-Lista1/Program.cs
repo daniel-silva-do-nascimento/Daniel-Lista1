f475ab3df9c6e44065b0b3e7e2dfc61c2d2dfc70
Aluno <Aluno@DESKTOP-O31M9M7>
2025-04-17 07:56:42 -0300

    Thouser 1000%

--- Daniel-Lista1/Program.cs
+++ Daniel-Lista1/Program.cs
@@ -16,7 +16,7 @@ namespace Daniel_Lista1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form19_Daniel());
+            Application.Run(new Form1());
         }
     }
 }
