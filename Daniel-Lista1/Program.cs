7cbd8ecf81fb386affedad868dd5bc2ebd238e43
Aluno <Aluno@DESKTOP-O31M9M7>
2025-04-17 07:51:07 -0300

    Thouser%

--- Daniel-Lista1/Program.cs
+++ Daniel-Lista1/Program.cs
@@ -16,7 +16,7 @@ namespace Daniel_Lista1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form18_Daniel());
+            Application.Run(new Form1());
         }
     }
 }
